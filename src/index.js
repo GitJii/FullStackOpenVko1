b9bf57b43f4d9fff2c62243496002c695033b746
Jaakko <jaska919@hotmail.com>
2018-08-16 10:40:08 +0300

    Tehtävä1.7
    Ei valmis versio, jotain häikkää esiin tuomisessa

--- src/index.js
+++ src/index.js
@@ -13,35 +13,37 @@ class App extends React.Component {
         }
     }
 
-
     annaHyva = () => {
-        this.setState({
-            hyva: this.state.hyva + 1, 
-            summa: this.state.summa + 1,
-            arivoidenlkm: this.state.arivoidenlkm + 1
-        })
+        return () => {
+            this.setState({
+                hyva: this.state.hyva + 1,
+                summa: this.state.summa + 1,
+                arivoidenlkm: this.state.arivoidenlkm + 1
+            })
+        }
     }
 
     annaNeutraali = () => {
-        this.setState({
-            neutraali: this.state.neutraali + 1,
-            summa: this.state.summa + 0,
-            arivoidenlkm: this.state.arivoidenlkm + 1
-        })
+        return () => {
+            this.setState({
+                neutraali: this.state.neutraali + 1,
+                summa: this.state.summa + 0,
+                arivoidenlkm: this.state.arivoidenlkm + 1
+            })
+        }
     }
 
     annaHuono = () => {
-        this.setState({
-            huono: this.state.huono + 1,
-            summa: this.state.summa - 1,
-            arivoidenlkm: this.state.arivoidenlkm +1
-        })
+        return () => {
+            this.setState({
+                huono: this.state.huono + 1,
+                summa: this.state.summa - 1,
+                arivoidenlkm: this.state.arivoidenlkm + 1
+            })
+        }
     }
-
-    keskiarvo =() => {
-        
+    keskiarvo = () => {
         return this.state.summa / this.state.arivoidenlkm
-
     }
 
     render() {
@@ -50,27 +52,56 @@ class App extends React.Component {
                 <div>
                     <h1>anna palautetta</h1>
 
-                    <button onClick={this.annaHyva}>Hyvä</button>
-                    <button onClick={this.annaNeutraali}>Neutraali</button>
-                    <button onClick={this.annaHuono}>Huono</button>
-                    
+                    <Button handleClick={this.annaHyva()}
+                        text="Hyvä"
+                    ></Button>
+
+                    <Button handleClick={this.annaNeutraali()}
+                        text="Neutraali"
+                    ></Button>
+
+                    <Button handleClick={this.annaHuono()}
+                        text="Huono"
+                    ></Button>
+
                     <h1>statistiikka</h1>
 
                     <div>
+                        
+                        
+                        {Statistic ({ teksti: 'Hyvä', tulos: this.state.hyva}) }
+                        
+                        
+                        
                         <p>Hyvä {this.state.hyva}</p>
                         <p>Neutraali {this.state.neutraali}</p>
                         <p>Huono {this.state.huono}</p>
                         <p>keskiarvo {this.keskiarvo()}</p>
-                        <p>positiivisia {100 * (this.state.hyva / this.state.arivoidenlkm)}</p>
+                        <p>positiivisia {100 * (this.state.hyva / this.state.arivoidenlkm)} %</p>
                     </div>
                 </div>
             </div>
         )
     }
 }
+
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+        {text}
+    </button>
+)
+
+const Statistics = () => (
+    <p>Statistiikka</p>
+)
+
+const Statistic = (teksti, tulos) => {
+    return  () => {
+        <p>{teksti} {tulos}</p>
+    }
+}
+
 ReactDOM.render(
     <App />,
     document.getElementById('root')
 );
-
-
